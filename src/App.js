66b208579b1259e2c38f00e79408136892c13499
Scott Penberthy <scott@scott.ai>
2020-05-17 23:15:55 -0700

    got the framework working.. now to flesh it out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import Header  from './components/Header';
+import Post from './components/Post';
+import Feed from './components/Feed';
+
+class App extends Component {
+
+    state =  {
+	articles: []
+    };
+
+    componentDidMount() {
+	fetch('https://us-central1-octo-news.cloudfunctions.net/articles')
+	  .then(res => res.json())
+          .then((data) => {
+            this.setState({ articles: data })
+	  })
+	 .catch(console.log)
+    };
+    
+ render() {
+   return <div className="App">
+      <Header  />
+      <section className="App-main">
+	 <Feed articles={this.state.articles} />
+      </section>
+    </div>;
+ };
+    
 }
 
 export default App;
